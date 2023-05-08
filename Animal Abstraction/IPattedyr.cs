1d7437e3c45a78b53c1dddbd67480d222c5d1c88
emils0 <89942504+emils0@users.noreply.github.com>
2023-05-08 09:54:59 +0200

    Done some interface stuff.

--- Animal Abstraction/IPattedyr.cs
+++ Animal Abstraction/IPattedyr.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Animal_Abstraction
 {
-	internal interface IPattedyr
-	{
-	}
+    internal interface IPattedyr
+    {
+
+    }
 }
